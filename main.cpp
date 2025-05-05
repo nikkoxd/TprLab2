a1403b3e3b7df6423069c65f4999064f7fe5325d
MiMoSiUm <141524250+MiMoSiUm@users.noreply.github.com>
2025-05-05 17:03:44 +0000

    it worbkskbskss!!!!

--- main.cpp
+++ main.cpp
@@ -8,28 +8,29 @@ using namespace std;
 
 int main()
 {
-    int i = 1;
-    double point[2] = {1, 1};
-    double gradient[2] = {200, 600};
-    double grModule = sqrt(200 * 200 + 600 * 600);
+// 100x_1^2 + 300x_2^2 + 5
+  int i = 1;
+  double point[2] = {1, 1};
+  double gradient[2] = {200 * point[0], 600 * point[1]};
+  double grModule = sqrt(pow(gradient[0], 2) + pow(gradient[1], 2));
 
-    while (grModule > E)
-    {
-        double h2 = pow(gradient[0], 2) * 100 + pow(gradient[1], 2) * 300;
-        double h = -(gradient[0] * point[0] * 2) * 100;
-        h -= gradient[1] * point[1] * 300;
-        double hval = (h2 * 2) / (-h);
+  while (grModule > E)
+  {
+    double h = point[0] * gradient[0] + 3 * point[1] * gradient[1];
+    h /= pow(gradient[0], 2) + 3 * pow(gradient[1], 2);
 
-        point[0] = point[0] - gradient[0] * hval;
-        point[1] = point[1] - gradient[1] * hval;
-        gradient[0] = 200 * point[0];
-        gradient[1] = 600 * point[1];
+    point[0] = point[0] - gradient[0] * h;
+    point[1] = point[1] - gradient[1] * h;
 
-        grModule = sqrt(pow(gradient[0], 2) + pow(gradient[1], 2));
-        cout << i++ << " " << grModule << endl;
-        // if (i > 10) break;
-    }
+    gradient[0] = 200 * point[0];
+    gradient[1] = 600 * point[1];
 
-    cout << "Minimum point: (" << point[0] << ", " << point[1] << ")\n";
-    cout << "Minimum: " << 100 * pow(point[0], 2) + 300 * pow(point[1], 2) + 5;
+    grModule = sqrt(pow(gradient[0], 2) + pow(gradient[1], 2));
+
+    cout << i++ << " " << grModule << endl;
+    // if (i > 10) break;
+  }
+
+  cout << "Minimum point: (" << point[0] << ", " << point[1] << ")\n";
+  cout << "Minimum: " << 100 * pow(point[0], 2) + 300 * pow(point[1], 2) + 5;
 }
